32baddb1c09f2b4c7c7a920832f15ab78687913e
Daniel Melzer <dm@digital-control.biz>
2012-06-22 17:48:40 +0200

    Registered class UrlShortener.

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@ ini_set('display_errors', true);
 
 require __DIR__ . '/../vendor/flight/Flight.php';
 require __DIR__ . '/../src/routes.php';
+require __DIR__ . '/../src/Damel/UrlShortener.php';
 
 Flight::set('flight.views.path', __DIR__ . '/../resource/template');
+Flight::register('shortener', '\Damel\UrlShortener');
 
 Flight::start();
