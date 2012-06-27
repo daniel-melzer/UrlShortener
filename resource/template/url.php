400386def4bc8e1ba2ee5258ecdfd7fc347f2466
Daniel Melzer <dm@digital-control.biz>
2012-06-27 11:47:29 +0200

    Added template url.php to display shortened URLs.

---
+++ resource/template/url.php
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="span6 offset3">
+		<h1>You're done</h1>
+		<p>Here is your new URL.</p>
+		<div class="control-group">
+			<input class="input-xlarge" type="text" value="<?=$shortUrl?>" readonly>
+		</div>
+	</div>
+</div>
+
