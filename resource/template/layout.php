3e7d5c3aec08d30fb28fccc8f91cc840ab0050f6
Daniel Melzer <dm@digital-control.biz>
2012-06-27 15:24:48 +0200

    Fixed path to assets.

--- resource/template/layout.php
+++ resource/template/layout.php
@@ -8,13 +8,13 @@
 		<meta name="author" content="">
 
 		<!-- Le styles -->
-		<link href="assets/css/bootstrap.css" rel="stylesheet">
+		<link href="/assets/css/bootstrap.css" rel="stylesheet">
 		<style>
 			body {
 				padding-top: 60px;
 			}
 		</style>
-		<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
 
 		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]>
@@ -22,11 +22,11 @@
 		<![endif]-->
 
 		<!-- Le fav and touch icons -->
-		<link rel="shortcut icon" href="assets/ico/favicon.ico">
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+		<link rel="shortcut icon" href="/assets/ico/favicon.ico">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/assets/ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/assets/ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/assets/ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="/assets/ico/apple-touch-icon-57-precomposed.png">
 	</head>
 
 	<body>
@@ -51,7 +51,7 @@
 		<div class="container">
 			<?=$content?>
 		</div>
-		<script src="assets/js/jquery.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="/assets/js/jquery.js"></script>
+		<script src="/assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
