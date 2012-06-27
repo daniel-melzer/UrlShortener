3a2f8563daa81f2e8829c3a48b58a531ecb879ee
Daniel Melzer <dm@digital-control.biz>
2012-06-27 13:24:41 +0200

    Added POST route for index page and form handling.

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,32 @@
 <?php
 //Index page.
-Flight::route('/', function() {
+Flight::route('GET /', function() {
 	Flight::render('index', array(), 'content');
+	Flight::render('layout', array());
+});
+
+Flight::route('POST /', function() {
+	$error = '';
+	$request = Flight::request();
+	$shortener = Flight::shortener();
+	try {
+		$result = $shortener->addUrl(Flight::request()->data->url, Flight::request()->data->code);
+	} catch(\InvalidArgumentException $e) {
+		$error = $e->getMessage();
+	}
+
+	if($result) {
+		$shortUrl = (isset($_SERVER['SERVER_PORT']) && (80 != $_SERVER['SERVER_PORT']) ?
+				'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . '/' . $result;
+		Flight::render('url', array(
+			'shortUrl' => $shortUrl
+		), 'content');
+	} else {
+		Flight::render('index', array(
+			'url' => Flight::request()->data->url,
+			'code' => Flight::request()->data->code,
+			'error' => $error
+		), 'content');
+	}
 	Flight::render('layout', array());
 });
