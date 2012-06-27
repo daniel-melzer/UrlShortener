22b18d9b356389aa086ae3694a40e5a3d8816aa5
Daniel Melzer <dm@digital-control.biz>
2012-06-27 17:57:53 +0200

    Added link to list page in navigation.

--- resource/template/layout.php
+++ resource/template/layout.php
@@ -32,16 +32,17 @@
 	<body>
 		<div class="navbar navbar-fixed-top">
 			<div class="navbar-inner">
-				<div class="container">
+				<div class="container offset2">
 					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</a>
-					<a class="brand" href="#">UrlShortener</a>
+					<a class="brand" href="/">UrlShortener</a>
 					<div class="nav-collapse">
 						<ul class="nav">
-							<li class="active"><a>Home</a></li>
+							<li<?php if(false === strpos($_SERVER['REQUEST_URI'], 'list')): ?> class="active"<?php endif; ?>><a href="/">Home</a></li>
+							<li<?php if(false !== strpos($_SERVER['REQUEST_URI'], 'list')): ?> class="active"<?php endif; ?>><a href="/list/1">List</a></li>
 						</ul>
 					</div>
 				</div>
