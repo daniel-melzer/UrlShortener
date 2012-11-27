cb5fea5c5e297466ee9133004dae11761b6231da
Daniel Melzer <daniel+github@melzer.ws>
2012-11-27 15:51:37 +0100

    Refactored route definitions..
    Moved logic to the new controller class.
    
     #14

--- src/routes.php
+++ src/routes.php
@@ -1,52 +1,24 @@
 <?php
 //404 page.
-Flight::map('notFound', function() {
-	Flight::render('404', array(), 'content');
-	Flight::render('layout', array());
+\Flight::map('notFound', function() {
+	\Flight::shortener()->notFoundAction();
 });
 
 //Index page.
-Flight::route('GET /', function() {
-	Flight::render('index', array(), 'content');
-	Flight::render('layout', array());
+\Flight::route('GET /', function() {
+	\Flight::shortener()->indexAction();
 });
 
-Flight::route('POST /', function() {
-	$request = Flight::request();
-	$shortener = Flight::shortener();
-	$result = $shortener->addUrl($request->data->url, $request->data->code);
-
-	if(is_string($result)) {
-		$shortUrl = (isset($_SERVER['SERVER_PORT']) && (80 != $_SERVER['SERVER_PORT']) ?
-				'https' : 'http') . '://' . $_SERVER['SERVER_NAME'] . '/' . $result;
-		Flight::render('url', array(
-				'shortUrl' => $shortUrl
-		), 'content');
-	} else {
-		Flight::render('index', array(
-				'url' => $request->data->url,
-				'code' => $request->data->code,
-				'errors' => $result
-		), 'content');
-	}
-	Flight::render('layout', array());
+\Flight::route('POST /', function() {
+	\Flight::shortener()->saveAction(\Flight::request());
 });
 
 //List pages.
 Flight::route('GET /list/@page:[0-9]+', function($page) {
-	$entriesPerPage = 25;
-	$page = Flight::shortener()->retrievePage($page, $entriesPerPage);
-	Flight::render('list', $page, 'content');
-	Flight::render('layout', array());
+	\Flight::shortener()->listAction($page);
 });
 
 //Redirect URL.
 Flight::route('GET /@code:[a-zA-Z0-9]{4,}', function($code) {
-	$shortener = Flight::shortener();
-	$url = $shortener->retrieveUrlByCode($code);
-	if(!empty($url)) {
-		Flight::redirect($url);
-	} else {
-		Flight::notFound();
-	}
+	\Flight::shortener()->redirectAction($code);
 });
