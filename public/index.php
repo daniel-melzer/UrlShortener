3e8aba5d1afeb693aed0fae97be30dc009cfb31e
Daniel Melzer <daniel+github@melzer.ws>
2012-11-29 14:52:13 +0100

    Removed definitions from index.php.
    Definitions are now in the bootstrap file.
    
    #12

--- public/index.php
+++ public/index.php
@@ -3,14 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', true);
 define('DOCUMENT_ROOT', __DIR__ . '/..');
 
-require DOCUMENT_ROOT . '/vendor/flight/Flight.php';
-require DOCUMENT_ROOT . '/app/config/routes.php';
-require DOCUMENT_ROOT . '/src/Damel/models/UrlShortener.php';
-require DOCUMENT_ROOT . '/src/Damel/controller/UrlShortenerController.php';
+require DOCUMENT_ROOT . '/app/app.php';
 
-Flight::set('flight.views.path', DOCUMENT_ROOT . '/app/resource/template');
-Flight::register('shortener', '\Damel\UrlShortenerController', array(
-		new \Damel\UrlShortener()
-));
-
-Flight::start();
+\Flight::loadConfig(CONFIG_DIR . '/config.ini');
+\Flight::start();
